db9ebd00f1fb0587d8a2b3a792488c9752f35251
Baitinq <manuelpalenzuelamerino@gmail.com>
2025-01-04 00:16:38 +0100

    another test

--- src/main.zig
+++ src/main.zig
@@ -31,3 +31,13 @@ test "simple test" {
     const res: *align(1) i32 = @ptrCast(hashtable.hashtable_get(ht, @constCast("key")));
     try std.testing.expectEqual(@as(i32, 4), res.*);
 }
+
+test "removing element" {
+    var ht = hashtable.hashtable_init();
+    defer _ = hashtable.hashtable_deinit(&ht);
+    const data: i32 = 4;
+    _ = hashtable.hashtable_put(ht, @constCast("key"), @constCast(&data));
+    _ = hashtable.hashtable_remove(ht, @constCast("key"));
+    const res: ?*anyopaque = hashtable.hashtable_get(ht, @constCast("key"));
+    try std.testing.expectEqual(null, res);
+}
